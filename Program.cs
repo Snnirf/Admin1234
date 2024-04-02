ad096dba99a8df489d7c52b6b180b87a4cfac256
Snnirf <165679497+Snnirf@users.noreply.github.com>
2024-04-02 15:20:15 +0900

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,45 @@
+﻿namespace Frinna_Candelaria_Number_1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string username = "Admin";
+
+            string password = "Admin1234";
+
+            int Tries = 3;
+
+            while (Tries >= 1)
+            {
+                Console.Write("Username : ");
+                string a = Console.ReadLine();
+
+                Console.Write("Password : ");
+                string b = Console.ReadLine();
+
+                if (a == username && b == password) break;
+                else
+                {
+                    Tries--;
+                    Console.WriteLine("Invalid Credentials");
+                    Console.WriteLine();
+
+                    if (Tries >= 1)
+                    {
+                        Console.WriteLine(Tries + " Please try again.");
+                        Console.WriteLine();
+                    }
+                }
+            }
+            if (Tries >= 1)
+            {
+                Console.WriteLine("Login Successful!");
+            }
+            else
+            {
+                Console.WriteLine("The maximum number of attempts has been reached.");
+            }
+        }
+    }
+}
---
+++ README.md
@@ -0,0 +1,2 @@
+# Admin
+ Repo for Admin
